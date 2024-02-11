0a465ac78712cda8f3ca6ff7418896877d5daa69
Benjamin Eder <eder.ben01@gmail.com>
2024-02-11 14:51:50 +0100

    feat: custom schema to use strings as primary keys

---
+++ lib/knight/schema.ex
@@ -0,0 +1,10 @@
+defmodule Knight.Schema do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+
+      @primary_key {:id, :string, []}
+      @foreign_key_type :string
+    end
+  end
+end
