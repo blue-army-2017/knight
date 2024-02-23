e32e7b97b3d3ae8e5e10536815d42e5edb30d87d
Benjamin Eder <eder.ben01@gmail.com>
2024-02-23 12:52:06 +0100

    chore: pico css

--- controller/routes.go
+++ controller/routes.go
@@ -5,7 +5,14 @@ import "net/http"
 func GetRoutesMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	mux.Handle("/static/", assetsHandler())
 	mux.HandleFunc("/health", getHealth)
 
 	return mux
+}
+
+func assetsHandler() http.Handler {
+	dir := http.Dir("assets/")
+	fs := http.FileServer(dir)
+	return http.StripPrefix("/static/", fs)
 }
