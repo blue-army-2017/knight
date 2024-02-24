04156fc3acadd082db3de37821e478865a86b7f8
Benjamin Eder <eder.ben01@gmail.com>
2024-02-24 22:09:12 +0100

    refactor: self-host material symbols

--- assets/download.sh
+++ assets/download.sh
@@ -2,5 +2,10 @@
 
 PICO_VERSION="2.0.3"
 
+mkdir -p assets/css
 # pico.css
 wget "https://raw.githubusercontent.com/picocss/pico/v${PICO_VERSION}/css/pico.blue.min.css" -O assets/css/pico.css
+
+mkdir -p assets/font
+# material-symbols.woff2
+wget "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsOutlined%5BFILL,GRAD,opsz,wght%5D.woff2" -O assets/font/material-symbols.woff2
