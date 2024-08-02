c8823d1c0682366aa181841f8000b93030c03b7c
Benjamin Eder <eder.ben01@gmail.com>
2024-08-02 15:39:45 +0200

    create simple member repository

--- model/members.go
+++ model/members.go
@@ -9,3 +9,22 @@ type Member struct {
 	LastName  string `gorm:"not null"`
 	Active    bool   `gorm:"not null"`
 }
+
+type MemberRepository interface {
+	FindAll() ([]Member, error)
+}
+
+type DefaultMemberRepository struct{}
+
+func NewMemberRepository() MemberRepository {
+	return &DefaultMemberRepository{}
+}
+
+func (r *DefaultMemberRepository) FindAll() ([]Member, error) {
+	var members []Member
+	result := db.
+		Order("last_name").
+		Order("first_name").
+		Find(&members)
+	return members, result.Error
+}
