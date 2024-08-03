23ad13405e99520cc45c5161f6e5eb44ac6fa645
Benjamin Eder <eder.ben01@gmail.com>
2024-08-03 23:52:06 +0200

    migrate legacy data

--- migration/main.go
+++ migration/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"time"
 
 	"github.com/blue-army-2017/knight/model"
 	"gorm.io/driver/sqlite"
@@ -11,15 +12,35 @@ import (
 	"gorm.io/gorm/clause"
 )
 
+var (
+	timeLocation *time.Location
+)
+
+func init() {
+	var err error
+	timeLocation, err = time.LoadLocation("Europe/Berlin")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	args := os.Args
 	dbFile := args[1]
 	dataFile := args[2]
 
+	migrate(dbFile, dataFile)
+}
+
+func migrate(dbFile, dataFile string) {
 	db, err := gorm.Open(sqlite.Open(dbFile), &gorm.Config{})
 	checkErr(err)
 
 	err = db.AutoMigrate(&model.Member{})
+	checkErr(err)
+	err = db.AutoMigrate(&model.Season{})
+	checkErr(err)
+	err = db.AutoMigrate(&model.SeasonGame{})
 	checkErr(err)
 
 	dataFileContent, err := os.ReadFile(dataFile)
@@ -38,11 +59,56 @@ func main() {
 		}
 		members = append(members, member)
 	}
+	// Members
 	result := db.
 		Clauses(clause.OnConflict{UpdateAll: true}).
 		Create(&members)
 	checkErr(result.Error)
 	log.Printf("Migrated %d members\n", result.RowsAffected)
+
+	var seasons []model.Season
+	var seasonGames []model.SeasonGame
+	for seasonId, seasonData := range data.Season {
+		season := model.Season{
+			ID:   seasonId,
+			Name: seasonData.Name,
+			Model: gorm.Model{
+				CreatedAt: seasonData.Created.In(timeLocation),
+			},
+		}
+		seasons = append(seasons, season)
+
+		for gameId, gameData := range seasonData.Games {
+			game := model.SeasonGame{
+				ID:       gameId,
+				Opponent: gameData.Opponent,
+				Home:     gameData.Home,
+				Mode:     gameData.Mode,
+				Date:     gameData.Date.In(timeLocation),
+				SeasonID: seasonId,
+			}
+
+			var presentMembers []model.Member
+			for _, memberId := range gameData.PresentMembers {
+				presentMembers = append(presentMembers, model.Member{ID: memberId})
+			}
+			game.PresentMembers = presentMembers
+
+			seasonGames = append(seasonGames, game)
+		}
+	}
+	// Seasons
+	result = db.
+		Clauses(clause.OnConflict{UpdateAll: true}).
+		Create(&seasons)
+	checkErr(result.Error)
+	log.Printf("Migrated %d seasons\n", result.RowsAffected)
+	// Season Games
+	result = db.
+		Clauses(clause.OnConflict{UpdateAll: true}).
+		Create(&seasonGames)
+	checkErr(result.Error)
+	log.Printf("Migrated %d season games\n", result.RowsAffected)
 }
 
 func checkErr(err error) {
