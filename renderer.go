bfa489727ae5ac049bdc662b134ebbaebc159e6d
Benjamin Eder <eder.ben01@gmail.com>
2024-09-03 21:24:03 +0200

    show logout button on prod

--- renderer.go
+++ renderer.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"html/template"
+	"os"
 	"slices"
 )
 
 func GetTemplateRenderer() *template.Template {
 	funcMap := template.FuncMap{
 		"containsString": slices.Contains[[]string, string],
+		"isProd":         isProd,
 	}
 
 	return template.Must(
 		template.New("").Funcs(funcMap).ParseGlob("view/**/*.html"),
 	)
+}
+
+func isProd() bool {
+	return os.Getenv("GIN_MODE") == "release"
 }
