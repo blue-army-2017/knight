106cbfb2d0941473a86bf56f079da3fbbc73b813
Benjamin Eder <eder.ben01@gmail.com>
2024-10-25 17:26:04 +0200

    run migrations on startup

--- controller/db.go
+++ controller/db.go
@@ -2,8 +2,14 @@ package controller
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
+	"log"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -14,10 +20,21 @@ func init() {
 	if !dbNameExists {
 		dbName = "knight.db"
 	}
+	log.Printf("Connecting to database %s", dbName)
 
 	var err error
 	db, err = sql.Open("sqlite3", dbName)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
+	log.Printf("Successfully connected to database %s", dbName)
+
+	m, err := migrate.New("file://schema", fmt.Sprintf("sqlite3://%s", dbName))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalln(err)
+	}
+	log.Println("Successfully applied migrations")
 }
