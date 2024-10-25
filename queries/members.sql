51f697292b076cd3bdb0b131ceae9e4abc0d9c77
Benjamin Eder <eder.ben01@gmail.com>
2024-10-25 09:22:47 +0200

    format sql

--- queries/members.sql
+++ queries/members.sql
@@ -1,14 +1,22 @@
 -- name: FindAllMembers :many
-SELECT *
-FROM members
-ORDER BY last_name, first_name;
+SELECT
+  *
+FROM
+  members
+ORDER BY
+  last_name,
+  first_name;
 
 -- name: FindMemberById :one
-SELECT *
-FROM members
-WHERE id = ?;
+SELECT
+  *
+FROM
+  members
+WHERE
+  id = ?;
 
 -- name: SaveMember :exec
-INSERT OR REPLACE
-INTO members (id, first_name, last_name, active)
-VALUES (?, ?, ?, ?);
+INSERT OR REPLACE INTO
+  members (id, first_name, last_name, active)
+VALUES
+  (?, ?, ?, ?);
